29d74a65875faf93f63fdce43622de725f3c7a6d
Tufail Ahmed <tufailahmedkhan@gmail.com>
2023-08-25 16:37:15 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringbootCustAppApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootCustAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCustAppApplication.class, args);
+	}
+		 @RequestMapping("/")
+	    public String home() {
+	        return "Welcome Here !! Spring Boot First Demo Shell";
+	    }
+
+}
